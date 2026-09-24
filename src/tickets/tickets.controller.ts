8c2c606e509eaca26d24ff52e4cd15443a41ef09
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-24 07:04:47 +0600

    feat: added get users by id logic

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TicketsService } from './tickets.service.js';
 
 @Controller('tickets')
@@ -8,5 +8,10 @@ export class TicketsController {
   @Get()
   findAll() {
     return this.ticketsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ticketsService.findOne(id);
   }
 }
