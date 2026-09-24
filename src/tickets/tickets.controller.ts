3a64f83155476fd3989256d99eb1fa1ac8ad969f
Sifat Bin Anwar <sifatshams@outlook.com>
2026-09-24 07:33:51 +0600

    feat: implemet @Query params and return to service

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { TicketsService } from './tickets.service.js';
+import { Ticket } from './ticket.interface.js';
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
   @Get()
-  findAll() {
+  findAll(@Query('status') status?: Ticket['status'], @Query('priority') priority?: Ticket['priority']) {
     return this.ticketsService.findAll();
   }
 
